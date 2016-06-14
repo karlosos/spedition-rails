257ab738dc9c7c7e524b1f29beebb40fc478dfb0
Karol Dzialowski <karlososhd@gmail.com>
2016-06-14 10:24:15 +0200

    Import klientow

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'clients.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Client.new
+  t.build_address
+  t.build_contact
+  t.name = row['name']
+  t.address.line1 = row['line1']
+  t.address.zip = row['zip']
+  t.address.state = row['state']
+  t.address.city = row['city']
+  t.address.country = row['country']
+  t.regon = row['regon']
+  t.pesel = row['pesel']
+  t.nip = row['nip']
+  t.invoice_currency = row['invoice_currency']
+  t.freight_currency = row['freight_currency']
+  t.payment_term = row['payment_term']
+  t.contact.phone1 = row['phone1']
+  t.contact.phone2 = row['phone2']
+  t.contact.fax = row['fax']
+  t.contact.email = row['email']
+  t.contact.www = row['www']
+  t.invoice_language = row['invoice_language']
+  t.save
+  puts "#{t.name} saved"
+end
+
+puts "There are now #{Client.count} rows in the transactions table"
