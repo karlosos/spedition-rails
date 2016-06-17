4dd06cc3bb3aa505b90831f10b273d9737a3aad0
Karol Dzialowski <karlososhd@gmail.com>
2016-06-17 13:40:49 +0200

    Usuniecie czulosci znakow przy wyszukiwaniu klientow ajax

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,7 +7,7 @@ class ClientsController < ApplicationController
     @clients = Client.joins(:address).search(params[:search]).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 30)
     respond_to do |format|
       format.html
-      format.json { render json: Client.joins(:address).where("name like ?", "%#{params[:q]}%").limit(2) }
+      format.json { render json: Client.joins(:address).where("lower(name) like ?", "%#{params[:q].downcase}%").limit(2) }
     end
   end
 
