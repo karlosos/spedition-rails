786d3c47954f4b593218cd322f1c8373542f84f7
Karol Dzialowski <karlososhd@gmail.com>
2016-06-20 13:29:15 +0200

    Testy integracyjne

--- app/models/invoice_name.rb
+++ app/models/invoice_name.rb
@@ -3,6 +3,8 @@ class InvoiceName < ActiveRecord::Base
     validates :month, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 13 }
     validates :year, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :number, uniqueness: { scope: [:month, :year] }
+    belongs_to :invoice
+
     def get_name
       return "#{number}/#{month}/#{year}"
     end
--- test/controllers/invoices_controller_test.rb
+++ test/controllers/invoices_controller_test.rb
@@ -1,49 +1,67 @@
 require 'test_helper'
 
 class InvoicesControllerTest < ActionController::TestCase
-  # setup do
-  #   @invoice = invoices(:one)
-  # end
-  #
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:invoices)
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
+  setup do
+    @invoice = invoices(:invoice_one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:invoices)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
   # test "should create invoice" do
   #   assert_difference('Invoice.count') do
-  #     post :create, invoice: {  }
+  #     post :create, invoice: { "date"=>"2016-06-21",
+  #       "invoice_name_attributes"=>
+  #           {
+  #             "number"=>"3",
+  #             "month"=>"6",
+  #             "year"=>"2016"
+  #           },
+  #       "client_id"=>"1",
+  #       "seller_id"=>"1",
+  #       "invoice_items_attributes"=>
+  #         {
+  #           "0"=>{"item_id"=>"1",
+  #             "quantity"=>"1",
+  #             "unit_price"=>"320.3",
+  #             "_destroy"=>"false"
+  #             }
+  #         }
+  #     }
   #   end
   #
   #   assert_redirected_to invoice_path(assigns(:invoice))
   # end
-  #
-  # test "should show invoice" do
-  #   get :show, id: @invoice
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit, id: @invoice
-  #   assert_response :success
-  # end
-  #
+
+  test "should show invoice" do
+    get :show, id: @invoice
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @invoice
+    assert_response :success
+  end
+
   # test "should update invoice" do
-  #   patch :update, id: @invoice, invoice: {  }
+  #   patch :update, id: @invoice, invoice: {}
+  #
   #   assert_redirected_to invoice_path(assigns(:invoice))
   # end
-  #
-  # test "should destroy invoice" do
-  #   assert_difference('Invoice.count', -1) do
-  #     delete :destroy, id: @invoice
-  #   end
-  #
-  #   assert_redirected_to invoices_path
-  # end
+
+  test "should destroy invoice" do
+    assert_difference('Invoice.count', -1) do
+      delete :destroy, id: @invoice
+    end
+
+    assert_redirected_to invoices_path
+  end
 end
