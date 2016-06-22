ec9db2c0258a09c95140246f8fce6b0a4f7450af
Karol Dzialowski <karlososhd@gmail.com>
2016-06-22 13:09:32 +0200

    Baza fix

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -39,7 +39,7 @@ class Invoice < ActiveRecord::Base
     elsif date_stop.present?
       where('clients.name LIKE ? AND date <= ?', "%#{client_name}%", "#{date_stop}")
     elsif date.present?
-      where('date == ? AND clients.name LIKE ?', "%#{DateTime.new(date)}%", "%#{client_name}%")
+      where('date == ? AND clients.name LIKE ?', DateTime.new(date), "%#{client_name}%")
     else
       where('clients.name LIKE ?', "%#{client_name}%")
     end
