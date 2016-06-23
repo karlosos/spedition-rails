6bcbeb966405db421332d70db6d5193e6ebc0ec7
Karol Dzialowski <karlososhd@gmail.com>
2016-06-23 16:01:58 +0200

    Poprawa testu jednostkowego faktur

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -16,11 +16,13 @@ class InvoiceTest < ActiveSupport::TestCase
   end
 
   test "invoice_name should be present" do
+    assert @invoice.invoice_name.get_name.present?
     @invoice.invoice_name = nil
     assert_not @invoice.valid?
   end
 
   test "date should be present" do
+    assert @invoice.date.present?
     @invoice.date = nil
     assert_not @invoice.valid?
   end
@@ -31,5 +33,35 @@ class InvoiceTest < ActiveSupport::TestCase
     end
     assert_equal 0, @invoice.invoice_items.size
     assert_not @invoice.valid?
+  end
+
+  test "invoice net price should be equal to sum of all invoice_items net prices" do
+    assert @invoice.invoice_items.size > 0
+    net_price = @invoice.net_price
+    sum_net_price = Money.new(0, 'EUR')
+    @invoice.invoice_items.each do |invoice_item|
+      sum_net_price += invoice_item.net_price
+    end
+    assert_equal net_price, sum_net_price
+  end
+
+  test "invoice value added tax should be equal to sum of all invoice_items vap" do
+    assert @invoice.invoice_items.size > 0
+    value_added_tax = @invoice.value_added_tax
+    sum_value_added_tax = Money.new(0, 'EUR')
+    @invoice.invoice_items.each do |invoice_item|
+      sum_value_added_tax += invoice_item.value_added_tax
+    end
+    assert_equal value_added_tax, sum_value_added_tax
+  end
+
+  test "invoice total price should be equal to sum of all invoice_items total price" do
+    assert @invoice.invoice_items.size > 0
+    total_selling_price = @invoice.total_selling_price
+    sum_total_selling_price = Money.new(0, 'EUR')
+    @invoice.invoice_items.each do |invoice_item|
+      sum_total_selling_price += invoice_item.total_selling_price
+    end
+    assert_equal sum_total_selling_price, total_selling_price
   end
 end
