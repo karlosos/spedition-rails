f84e1aa1e5141fd61a79c53e796156eb24bbd64c
Karol Dzialowski <karlososhd@gmail.com>
2016-06-30 13:20:57 +0200

    Panel edycyjny faktur

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,10 @@
 module InvoicesHelper
+  def link_to_menu_invoice(text, glyphicon_class, link, delete = 0)
+    html = "<span class='glyphicon #{glyphicon_class}' aria-hidden='true'></span> #{text}"
+    if delete == 1
+      link_to(raw(html), link, method: :delete, data: { confirm: 'Are you sure?' } )
+    else
+      link_to(raw(html), link)
+    end
+  end
 end
