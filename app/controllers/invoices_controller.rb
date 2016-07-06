332b444a3c27169d2047409da9770e8b7fa0685d
Karol Dzialowski <karlososhd@gmail.com>
2016-07-06 16:05:02 +0200

    Kierunek domyslnego sortowania

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -164,6 +164,6 @@ class InvoicesController < ApplicationController
     end
 
     def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
