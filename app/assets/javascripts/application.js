53989dd76a07ec98f0a9adaf62eb57e8de7e3455
Karol Dzialowski <karlososhd@gmail.com>
2016-07-11 20:08:57 +0200

    Przeniesienie js do coffeescript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,87 +22,6 @@
 //= require select2_locale_pl
 //= require moment.min.js
 
-$(document).ready(function() {
-  // client
-  $(document).bind("ajax:success", 'form#new_client', function(evt, data, status, xhr) {
-    if ( evt.target.id == "new_client" ) {
-      var $form = $(this);
-      var option = $('<option selected>' + data.name + '</option>').val("" + data.id);
-      $("#invoice_client_id").append(option).trigger('change');
-      $('#new_client_modal').modal_success();
-      console.log("klient")
-    } else if (evt.target.id == "new_item") {
-      var $form = $(this);
-      $('#new_item_modal').modal_success();
-      console.log("item")
-    }
-  })
-
-  $(document).bind('ajaxError', 'form#new_client', function(event, jqxhr, settings, exception) {
-    $(event.data).render_form_errors($.parseJSON(jqxhr.responseText));
-  });
-
-  $('#new_client_modal').on('shown.bs.modal', function() {
-    $('#client_name').focus()
-  })
-
-  $('#new_item_modal').on('shown.bs.modal', function() {
-    $('#item_name').focus()
-  })
-});
-
-(function($) {
-
-  $.fn.modal_success = function() {
-    // close modal
-    this.modal('hide');
-
-    // clear form input elements
-    // todo/note: handle textarea, select, etc
-    this.find('form input[type="text"]').val('');
-
-    // clear error state
-    this.clear_previous_errors();
-  };
-
-  $.fn.render_form_errors = function(errors) {
-
-    console.log('chce rendererowarac errory');
-
-    $form = this;
-    this.clear_previous_errors();
-    model = this.data('model');
-
-    console.log(model)
-      //client_contact_attributes_fax
-      // show error messages in input form-group help-block
-    $.each(errors, function(field, messages) {
-      console.log(field)
-      if (field.search('[.]') > 0) {
-        association = field.substring(0, field.search('[.]'));
-        association = association + "_attributes";
-        attribute = field.substring(field.search('[.]') + 1);
-        $input = $('input[name="' + model + '[' + association + ']' + '[' + attribute + ']"]');
-        console.log('input[name="' + model + '[' + association + ']' + '[' + attribute + ']"]');
-        $input.closest('.form-group').addClass('has-error').find('.help-block').html(messages.join(' & '));
-      } else {
-        $input = $('input[name="' + model + '[' + field + ']"]');
-        console.log('input[name="' + model + '[' + field + ']"]')
-        $input.closest('.form-group').addClass('has-error').find('.help-block').html(messages.join(' & '));
-      }
-    });
-
-  };
-
-  $.fn.clear_previous_errors = function() {
-    $('.form-group.has-error', this).each(function() {
-      $('.help-block', $(this)).html('');
-      $(this).removeClass('has-error');
-    });
-  }
-
-}(jQuery));
-
 
 function show_client_modal() {
   $('#new_client_modal').modal('toggle')
@@ -111,5 +30,5 @@ function show_client_modal() {
 
 function show_item_modal() {
   $('#new_item_modal').modal('toggle')
-  $('#invoice_client_id').select2("close")
+  $('.invoice_items_form').find('select').select2("close")
 }
