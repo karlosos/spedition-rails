7a0d0757c60494cd07e18f2c6a997a951dfe91f7
Karol Dzialowski <karlososhd@gmail.com>
2016-07-20 16:42:41 +0200

    Link_to_menu w jednym helperze

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,4 +50,13 @@ module ApplicationHelper
      return "btn-danger"
    end
  end
+
+ def link_to_menu(text, glyphicon_class, link, delete = 0)
+   html = "<span class='glyphicon #{glyphicon_class}' aria-hidden='true'></span> #{text}"
+   if delete == 1
+     link_to(raw(html), link, method: :delete, data: { confirm: 'Are you sure?' } )
+   else
+     link_to(raw(html), link)
+   end
+ end
 end
--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,10 +1,3 @@
 module ClientsHelper
-  def link_to_menu_client(text, glyphicon_class, link, delete = 0)
-    html = "<span class='glyphicon #{glyphicon_class}' aria-hidden='true'></span> #{text}"
-    if delete == 1
-      link_to(raw(html), link, method: :delete, data: { confirm: 'Are you sure?' } )
-    else
-      link_to(raw(html), link)
-    end
-  end
+
 end
