71a1cf945b0f74c151352223b04675233788c699
Karol Dzialowski <karlososhd@gmail.com>
2016-07-25 18:23:35 +0200

    Testy jednostkowe zlecenia

--- app/models/transport_order.rb
+++ app/models/transport_order.rb
@@ -6,4 +6,15 @@ class TransportOrder < ActiveRecord::Base
   monetize :freight_rate_cents, :numericality => {
     :greater_than_or_equal_to => 0
   }
+
+  validates :transport_order_name, presence: true
+  validates :carrier, presence: true
+  validates :client, presence: true
+  validates :loading_city, presence: true
+  validates :loading_country, presence: true
+  validates :loading_zip, presence: true
+  validates :unloading_city, presence: true
+  validates :unloading_country, presence: true
+  validates :unloading_zip, presence: true
+  validates :route, presence: true
 end
--- test/models/transport_order_test.rb
+++ test/models/transport_order_test.rb
@@ -10,4 +10,59 @@ class TransportOrderTest < ActiveSupport::TestCase
     @transport_order.transport_order_name = nil
     assert_not @transport_order.valid?
   end
+
+  test "carrier should be present" do
+    assert @transport_order.carrier.present?
+    @transport_order.carrier = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "client should be present" do
+    assert @transport_order.client.present?
+    @transport_order.client = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "route should be present" do
+    assert @transport_order.route.present?
+    @transport_order.route = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "loading country should be present" do
+    assert @transport_order.loading_country.present?
+    @transport_order.loading_country = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "loading zip should be present" do
+    assert @transport_order.loading_zip.present?
+    @transport_order.loading_zip = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "loading city should be present" do
+    assert @transport_order.loading_city.present?
+    @transport_order.loading_city = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "unloading country should be present" do
+    assert @transport_order.unloading_country.present?
+    @transport_order.unloading_country = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "unloading zip should be present" do
+    assert @transport_order.unloading_zip.present?
+    @transport_order.unloading_zip = nil
+    assert_not @transport_order.valid?
+  end
+
+  test "unloading city should be present" do
+    assert @transport_order.unloading_city.present?
+    @transport_order.loading_city = nil
+    assert_not @transport_order.valid?
+  end
+
 end
