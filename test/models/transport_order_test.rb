86db1fde928d648bf0c5c52110e30c350ce9c64c
Karol Dzialowski <karlososhd@gmail.com>
2016-08-15 17:13:13 +0200

    Testy stalych danych klienta i carriera w zleceniu

--- test/models/transport_order_test.rb
+++ test/models/transport_order_test.rb
@@ -44,4 +44,169 @@ class TransportOrderTest < ActiveSupport::TestCase
     assert_equal 0, @transport_order.unloading_places.size
     assert_not @transport_order.valid?
   end
+
+  test "client name should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_name, @transport_order.client.name
+
+    @client.name = "New client name"
+    @client.save
+    assert_not_equal @transport_order.client_name, @transport_order.client.name
+  end
+
+  test "client nip should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_nip, @transport_order.client.nip
+
+    @client.nip = "New client nip"
+    @client.save
+    assert_not_equal @transport_order.client_nip, @transport_order.client.nip
+  end
+
+  test "client street should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_street, @transport_order.client.address.street
+
+    @client.address.street = "New client street"
+    @client.save
+    assert_not_equal @transport_order.client_street, @transport_order.client.address.street
+  end
+
+  test "client zip should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_zip, @transport_order.client.address.zip
+
+    @client.address.zip = "New client zip"
+    @client.save
+    assert_not_equal @transport_order.client_zip, @transport_order.client.address.zip
+  end
+
+  test "client city should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_city, @transport_order.client.address.city
+
+    @client.address.city = "New client city"
+    @client.save
+    assert_not_equal @transport_order.client_city, @transport_order.client.address.city
+  end
+
+  test "client country should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_country, @transport_order.client.address.country
+
+    @client.address.country = "New client country"
+    @client.save
+    assert_not_equal @transport_order.client_country, @transport_order.client.address.country
+  end
+
+  test "client email should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_email, @transport_order.client.contact.email
+
+    @client.contact.email = "New client email"
+    @client.save
+    assert_not_equal @transport_order.client_email, @transport_order.client.contact.email
+  end
+
+  test "client phone should be fixed" do
+    @client = clients(:client_microsoft)
+    @transport_order.client = @client
+    @transport_order.save
+    assert_equal @transport_order.client_phone, @transport_order.client.contact.phone1
+
+    @client.contact.phone1 = "New client phone"
+    @client.save
+    assert_not_equal @transport_order.client_phone, @transport_order.client.contact.phone1
+  end
+
+  test "carrier name should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_name, @transport_order.carrier.carrier_name
+
+    @carrier.carrier_name = "New carrier name"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_name, @transport_order.carrier.carrier_name
+  end
+
+  test "carrier nip should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_nip, @transport_order.carrier.nip
+
+    @carrier.nip = "New carrier nip"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_nip, @transport_order.carrier.nip
+  end
+
+  test "carrier driver name should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_driver_name, @transport_order.carrier.driver_name
+
+    @carrier.driver_name = "New carrier nip"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_driver_name, @transport_order.carrier.driver_name
+  end
+
+  test "carrier street should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_street, @transport_order.carrier.address.street
+
+    @carrier.address.street = "New carrier street"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_street, @transport_order.carrier.address.street
+  end
+
+  test "carrier zip should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_zip, @transport_order.carrier.address.zip
+
+    @carrier.address.zip = "New carrier zip"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_zip, @transport_order.carrier.address.zip
+  end
+
+  test "carrier city should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_city, @transport_order.carrier.address.city
+
+    @carrier.address.city = "New carrier city"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_city, @transport_order.carrier.address.city
+  end
+
+  test "carrier country should be fixed" do
+    @carrier = carriers(:carrier_one)
+    @transport_order.carrier = @carrier
+    @transport_order.save
+    assert_equal @transport_order.carrier_country, @transport_order.carrier.address.country
+
+    @carrier.address.country = "New carrier country"
+    @carrier.save
+    assert_not_equal @transport_order.carrier_country, @transport_order.carrier.address.country
+  end
 end
