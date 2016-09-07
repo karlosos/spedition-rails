9db160fd0857c67e2f87b31706544a61048fca63
Karol Dzialowski <karlososhd@gmail.com>
2016-09-07 15:34:34 +0200

    Dodanie uzytkownikow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905141736) do
+ActiveRecord::Schema.define(version: 20160907132657) do
 
   create_table "addresses", force: true do |t|
     t.string   "line1"
@@ -313,5 +313,23 @@ ActiveRecord::Schema.define(version: 20160905141736) do
   end
 
   add_index "unloading_places", ["transport_order_id"], name: "index_unloading_places_on_transport_order_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
