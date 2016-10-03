830f56472c968cd9ba89387e0ca329c3db02ee81
Karol Dzialowski <karlososhd@gmail.com>
2016-10-03 16:53:39 +0200

    Tworzenie nowe konta przy omniath i logowanie sie na nie

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
-      if @user.persisted?
+      if @user.present?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in_and_redirect @user, :event => :authentication
       else
